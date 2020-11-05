688fbd514a6f9ee39112f46c80901c5f9725a0da
Jaspreet Singh <dexter@Jaspreet-SinghB350-MacBook-Pro.local>
2020-11-05 18:03:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import Amplify from "aws-amplify";
+import { AmplifySignOut, withAuthenticator } from "@aws-amplify/ui-react";
+
+import gql from "graphql-tag";
+import client from "./client";
+import { listTodos } from "./graphql/queries";
+
+import awsconfig from "./aws-exports";
+
+Amplify.configure(awsconfig);
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    client
+      .query({
+        query: gql(listTodos),
+      })
+      .then(({ data: { listTodos } }) => {
+        // todos = listTodos.items;
+        setTodos(listTodos.items);
+      });
+  }, [todos]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <AmplifySignOut />
+        {todos.map((item) => {
+          return (
+            <div key={item.id} style={{display:"flex", justifyContent: "center"}}>
+              <h2>{item.name}</h2>&nbsp;&nbsp;
+              <h2>{item.complete ? "completed" : "pending"}</h2>
+            </div>
+          );
+        })}
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
